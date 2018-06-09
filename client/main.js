37783e3783bac2057b44edc629f0396800f20582
Nicholas Van Keulen <colevank@gmail.com>
2018-06-09 14:34:32 -0600

    Output now works though HTML

--- client/main.js
+++ client/main.js
@@ -16,36 +16,67 @@ Template.hello.helpers({
 
 Template.hello.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
+    // increment the counter wwaterhen button is clicked
     instance.counter.set(instance.counter.get() + 1);
-  },
+  },  
+});  
+
+Template.beerIngredients.onCreated(function() {
+
+  this.ingredientsNeeded = new ReactiveVar();
+  this.water = new ReactiveVar();
+  this.sugar = new ReactiveVar();
+  this.yeast = new ReactiveVar();
+  this.grain = new ReactiveVar();
+
+});
+
+Template.beerIngredients.helpers({ 
+  ingredientsNeeded(){
+    var needed =Template.instance().ingredientsNeeded.get();
+    console.log(`rendering ingredients needed: ${needed}`)
+    return needed;
+},
+  water() { return Template.instance().water.get(); },
+  yeast() { return Template.instance().yeast.get(); },
+  grain() { return Template.instance().grain.get(); },
+  sugar() { return Template.instance().sugar.get(); },
 });
 
 Template.beerIngredients.events({
   'click #submit': function (event, instance) {
+    event.preventDefault();
     
-
     var grain = +$("#grain").val();
     var water = +$("#water").val();
     var sugar = +$("#sugar").val();
     var yeast = +$("#yeast").val();
-
+    
     console.log("Adding data: " + grain + ", " + water + ", " + sugar + ", " + yeast);
-
+    
     var reps = 0;
     if (grain > 0) { reps = grain / 5.0 }
     else if (water > 0) { reps = water / 6.0 }
     else if (yeast > 0) { reps = yeast / 2.0 }
     else if (sugar > 0) { reps = sugar / 2.0 }
-
+    
+    //What does this do?
     reps = Math.floor(reps)
-
+    
     //var baseAmount = 0;
     var grainNeeded = reps * 5;
     var waterNeeded = reps * 6;
     var yeastNeeded = reps * 2;
     var sugurNeeded = reps;
 
+    var ingredientsNeeded = `Grain: ${grainNeeded}, Water: ${waterNeeded}, Sugar: ${sugurNeeded}, Yeast: ${yeastNeeded}`
+
+    Template.instance().ingredientsNeeded.set(ingredientsNeeded);
+    Template.instance().grain.set(grainNeeded)
+    Template.instance().yeast.set(yeastNeeded)
+    Template.instance().water.set(waterNeeded)
+    Template.instance().sugar.set(sugurNeeded)
+    
     /* if (grain > 0) {
       grainNeeded = grain;
       baseAmount = grain / 5;
@@ -73,23 +104,26 @@ Template.beerIngredients.events({
       sugurNeeded = baseAmount;
       waterNeeded = baseAmount * 6;
       grainNeeded = baseAmount * 5;
-
+      
     } else {
       console.log("Please provide input > 0")
-    }
- */
-
-    /* Causes ReferenceError: Console is not defined;
-    Console.log("Can make ${reps} batches of beer.") */
-    
+    }  
+    */ 
+   
+   /* Causes ReferenceError: Console is not defined;
+   Console.log("Can make ${reps} batches of beer.") */
+   /**
+    * 
     console.log("This make a minumum beer amout of: " + reps)
     console.log("Grain needed: " + grainNeeded)
     console.log("Mineral Water needed: " + waterNeeded)
     console.log("Sugar needed: " + sugurNeeded)
     console.log("Leavening Agent needed: " + yeastNeeded)
+    */
+   
+   
+   
+   
+  } 
+});  
 
-
-
-
-  }
-});
